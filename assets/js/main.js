b992ad2d6df0124c96da8d2665b93a06192778c8
rathanont <rathanont.s@gmail.com>
2019-07-05 14:02:05 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,27 @@
-$(function () {
+$(function() {
   // Toggle search
   $("#formSearch").hide();
-  $("#toggleSearch").click(function () {
+  $("#toggleSearch").click(function() {
     $("#formSearch").toggle();
     $(this).hide();
   });
 
   // Banner slide
-  $('#bannerSlide .banner').on('init', function (e, slick) {
-    var $firstAnimatingElements = $('div.banner-item:first-child').find('[data-animation]');
+  $("#bannerSlide .banner").on("init", function(e, slick) {
+    var $firstAnimatingElements = $("div.banner-item:first-child").find(
+      "[data-animation]"
+    );
     doAnimations($firstAnimatingElements);
   });
-  $('#bannerSlide .banner').on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-    var $animatingElements = $('div.banner-item[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+  $("#bannerSlide .banner").on("beforeChange", function(
+    e,
+    slick,
+    currentSlide,
+    nextSlide
+  ) {
+    var $animatingElements = $(
+      'div.banner-item[data-slick-index="' + nextSlide + '"]'
+    ).find("[data-animation]");
     doAnimations($animatingElements);
   });
   $("#bannerSlide .banner").slick({
@@ -20,25 +29,28 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 10000,
     dots: true,
-    responsive: [{
-      breakpoint: 992,
-      settings: {
-        arrows: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false
+        }
       }
-    }]
+    ]
   });
 
   function doAnimations(elements) {
-    var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    elements.each(function () {
+    var animationEndEvents =
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    elements.each(function() {
       var $this = $(this);
-      var $animationDelay = $this.data('delay');
-      var $animationType = 'animated ' + $this.data('animation');
+      var $animationDelay = $this.data("delay");
+      var $animationType = "animated " + $this.data("animation");
       $this.css({
-        'animation-delay': $animationDelay,
-        '-webkit-animation-delay': $animationDelay
+        "animation-delay": $animationDelay,
+        "-webkit-animation-delay": $animationDelay
       });
-      $this.addClass($animationType).one(animationEndEvents, function () {
+      $this.addClass($animationType).one(animationEndEvents, function() {
         $this.removeClass($animationType);
       });
     });
